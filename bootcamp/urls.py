f712688939e793e4f4b31492cb54c1262ea26234
lichun <i@lichun.me>
2018-07-27 14:57:40 +0800

    Change urls use new path function.

--- bootcamp/urls.py
+++ bootcamp/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path('login', auth_views.login,
          {'template_name': 'core/cover.html'}, name='login'),
     path('logout', auth_views.logout, {'next_page': '/'}, name='logout'),
-    path('signup/$', authentication_views.signup, name='signup'),
+    path('signup/', authentication_views.signup, name='signup'),
 
     path('feeds/', include('bootcamp.feeds.urls')),
     path('settings/', include('bootcamp.core.urls')),
